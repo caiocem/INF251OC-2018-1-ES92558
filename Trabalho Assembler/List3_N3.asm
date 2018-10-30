e50eb817378fa7ae28ca2dae036e294e5b59a9c5
caiocem <43625366+caiocem@users.noreply.github.com>
2018-10-30 13:56:57 -0300

    Update List3_N3.asm

--- Trabalho Assembler/List3_N3.asm
+++ Trabalho Assembler/List3_N3.asm
@@ -1,8 +1,9 @@
+#Matricula 92558
 lw $t2,0($gp) #ler A
 lw $t3,8($gp) #ler B
 li $t4,0      #0 auxiliar
-beq $t2,$zero,Fim #A é 0? se sim terminar
-slt $t7,$zero,$t2  #A é menor q 0?
+beq $t2,$zero,Fim #A Ã© 0? se sim terminar
+slt $t7,$zero,$t2  #A Ã© menor q 0?
 beq $t7,$zero,Anegativo #Caso A < 0, ir para Anegativo
 Loop: add $t4,$t4,$t3 #Caso A > 0, incrementar B em t4
 addi $t2,$t2,-1 #decrementar A em 1
